0419e37bd7cafb0944fa3abcd9ae8c1d437b3978
HoangTN <HoangTN@DESKTOP-H68FGFI>
2021-08-22 17:12:22 +0700

    Initial commit

---
+++ src/arraydemo/ArrayDemo.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arraydemo;
+
+/**
+ *
+ * @author HoangTN
+ */
+public class ArrayDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+                // Định nghĩa một mảng các số nguyên
+        int[] anArray;
+
+        // chỉ định mảng có 10 phần tử
+        anArray = new int[10];
+           
+        // khởi gán phần tử đầu tiên của mảng
+        anArray[0] = 100;
+        // khởi gán phần tử thứ 2 của mảng
+        anArray[1] = 200;
+        // và các phần tử khác
+        anArray[2] = 300;
+        anArray[3] = 400;
+        anArray[4] = 500;
+        anArray[5] = 600;
+        anArray[6] = 700;
+        anArray[7] = 800;
+        anArray[8] = 900;
+        anArray[9] = 1000;
+
+        System.out.println("Element at index 0: "
+                           + anArray[0]);
+        System.out.println("Element at index 1: "
+                           + anArray[1]);
+        System.out.println("Element at index 2: "
+                           + anArray[2]);
+        System.out.println("Element at index 3: "
+                           + anArray[3]);
+        System.out.println("Element at index 4: "
+                           + anArray[4]);
+        System.out.println("Element at index 5: "
+                           + anArray[5]);
+        System.out.println("Element at index 6: "
+                           + anArray[6]);
+        System.out.println("Element at index 7: "
+                           + anArray[7]);
+        System.out.println("Element at index 8: "
+                           + anArray[8]);
+        System.out.println("Element at index 9: "
+                           + anArray[9]);
+    }
+} 
+
+    
+    
+
